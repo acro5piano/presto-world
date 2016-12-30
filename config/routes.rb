651e1ed3511a4e334516d31b8e49db3c84ea00ca
gosho-kazuya <gosho-kazuya@919.jp>
2016-12-30 17:29:24 +0000

    Create landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Landing page
   root 'static_pages#index'
+  get '/register', to: 'static_pages#index', as: 'register'
+  get '/login', to: 'static_pages#index', as: 'login'
+  get '/logout', to: 'static_pages#index', as: 'logout'
 end
