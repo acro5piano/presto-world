a0508c401d7200d54e80f4089340278684f5bd90
gosho-kazuya <gosho-kazuya@919.jp>
2017-01-02 02:17:57 +0000

    add current_teacher

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,9 @@ module SessionsHelper
   def logged_in?
     session[:teacher_id] != nil
   end
+
+  def current_teacher
+    return nil if !session[:teacher_id]
+    Teacher.find(session[:teacher_id])
+  end
 end
