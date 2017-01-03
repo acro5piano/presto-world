4779be06352c386da759a118377a8a29354c35d7
gosho-kazuya <gosho-kazuya@919.jp>
2017-01-03 00:37:27 +0000

    change table name teacher => user

--- db/migrate/20161229225631_create_reviews.rb
+++ db/migrate/20161229225631_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
     create_table :reviews do |t|
-      t.integer :teacher_id
+      t.integer :user_id
       t.integer :student_id
       t.integer :star
       t.string :title
