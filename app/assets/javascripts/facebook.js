334f4ba4d8e98be4953d699d0066d154f6d3644d
gosho-kazuya <gosho-kazuya@919.jp>
2017-01-03 20:09:11 +0000

    get facebook app id from env vars

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -1,23 +1,25 @@
-// TODO: 開発環境とそれ以外で分ける
-var appId = '1252598184786360';
-
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : appId,
-    xfbml      : true,
-    version    : 'v2.8'
-  });
-};
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.8&appId=" + appId;
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 $(document).on('ready', function() {
+  var appId = $.variables.facebook_app_id
+
+  // Facebook SDK
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : appId,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.8&appId=" + appId;
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+
+  // Login and Register
   $('.fb-register').on('click', function() {
     FB.getLoginStatus(function(response) {
       if (response.status === 'connected'){
